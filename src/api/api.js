1ba00cd8881bc488c1a2e2739562a111d2790a05
mluqman <you@example.com>
2026-07-16 13:20:45 +0500

    api fixd

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://banking-backend-orcin.vercel.app",
+  baseURL: "https://banking-backend-3xewf0h5l-mluqman-mains-projects.vercel.app",
   withCredentials: true,
 });
